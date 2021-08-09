cc311f8763f057b4afa4c663d7395464c7df05ee
ingsgreat <1209455541@qq.com>
2021-08-09 15:40:20 +0800

    Initial commit

---
+++ src/main/java/com/example/yhsystem/YhsystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.yhsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YhsystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YhsystemApplication.class, args);
+    }
+
+}
